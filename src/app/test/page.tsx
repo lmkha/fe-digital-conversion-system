c46ccdf3d1311285b549233dc881d96d513b1441
Le Minh Kha <lmkha0201@gmail.com>
2024-11-13 21:26:57 +0700

    fix build error

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,49 +1,7 @@
-'use client';
-
-import * as React from 'react';
-import dayjs, { Dayjs } from 'dayjs';
-import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { DatePicker as MUIDatePicker } from '@mui/x-date-pickers/DatePicker';
-import { Box } from '@mui/material';
-
-export default function TestPage() {
+export default function Page() {
     return (
         <>
-            <Box sx={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: '100vh',
-            }}>
-                <DatePicker />
-            </Box>
+            <h1>Test Page</h1>
         </>
-    );
+    )
 }
-
-export function DatePicker() {
-    const [value, setValue] = React.useState<Dayjs | null>(dayjs());
-
-    return (
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DemoContainer components={['DatePicker', 'DatePicker']}>
-                <MUIDatePicker
-                    label="Controlled picker"
-                    value={value}
-                    onChange={(newValue) => setValue(newValue)}
-                    slotProps={{
-                        textField: {
-                            size: 'small',
-                            sx: {
-                                width: '100%'
-                            }
-                        },
-                    }}
-                />
-            </DemoContainer>
-        </LocalizationProvider>
-    );
-}
-
